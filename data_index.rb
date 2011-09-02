4ac36418f0004d250b1827d73de2aafa6fbf52ac
Arthur Guimarães <warthur2@gmail.com>
2011-09-02 11:28:55 -0300

    Le arquivo e cria a tabela hash

--- data_index.rb
+++ data_index.rb
@@ -1,24 +1,48 @@
-class parser_receiver (file)
+class Parser_receiver
 	attr_accessor :list, :hash_table
 # Trabalha com a construção da tabela hash a partir dos arquivos criados pelo parser
 
-	def initialize
+	def initialize(file)
 		if File.exist?(file)
-            @list = File.open(file)
-        end
+      @list = File.open(file)
+    end
 	end
 
     def init_hash
-        hash_table = Hash.new
+        @hash_table = Hash.new
 
         @list.each_line do |i|
             aux = i.split(' ')
-            hash_table[aux.delete_at(0)] = aux
-        end     
-                
-        return hash_table
+            # indice -> [quantidade, [posição,documento], [posição, documento], ...]
+           if @hash_table.has_key?(aux.first) == false 
+              @hash_table[aux.first] = [[aux[1].to_i,aux[2].to_i]]
+              @hash_table[aux.first].insert(0,1)
+              #testes
+                puts "Entrada na tabela #{aux.first} => #{@hash_table[aux.first]}"
+                sleep 0.9
+           else
+              #testes
+                puts "entrou no else #{aux.first} ja inserido 1 vez"
+              @hash_table[aux.first].push([aux[1].to_i, aux[2].to_i])
+              @hash_table[aux.first][0] = @hash_table[aux.first][0] + 1
+              
+              #teste
+              puts "Tabela mudou para => #{@hash_table[aux.first]}"
+
+           end     
+        end
+        return @hash_table
     end
-        
-    
+end
+
+def main
+  
+  hash_table = Parser_receiver.new("entrada.txt").init_hash
+  
+  #testes
+    puts "Tabela de indices criada com sucesso!"
+    puts "Search \"the\" #{hash_table["the"][1]}"
 
 end
+
+main()

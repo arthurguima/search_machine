3d59bac3da334c7ab1b6d3c89db1508d4c9cb46e
Arthur Guimarães <warthur2@gmail.com>
2011-10-21 16:17:05 -0200

    Inicio Refatoração

--- data_index.rb
+++ data_index.rb
@@ -9,18 +9,35 @@ class Data_index
     @hash_table = Hash.new
 	end
 
-    def init_hash
-        @list.each_line do |i|
-            aux = i.split(' ')
-            # hash[termo] = [qtd, hash[documento]]; hash[documento] = [posição1, posição2...]
-           if @hash_table.has_key?(aux.first) == false 
-              @hash_table[aux.first] = [ Hash[aux[2].to_i , [aux[1].to_i]]]
-              @hash_table[aux.first].insert(0,1)
-           else
-              @hash_table[aux.first][1].has_key?(aux[2].to_i) ? @hash_table[aux.first][1][aux[2].to_i].push(aux[1.to_i]) : @hash_table[aux.first][1][aux[2].to_i] = [aux[1].to_i]
-              @hash_table[aux.first][0] = @hash_table[aux.first][0] + 1
-           end     
+
+   # Inicia o Hash a partir do arquivo de entrada file
+   # Retorna o indice invertido; Tipo: Hash
+   # hash[termo] = [qtd, hash[documento]]; hash[documento] = [qtd, posição1, posição2...
+   def init_hash
+      @list.each_line do |i|
+        
+        aux = i.split(' ') #
+
+        if @hash_table.has_key?(aux.first) == false #Se o termo ainda não existe no indice
+           
+           # Adiciona o termo ao hash; Valor = novo Hash[docID] = [pos]
+           @hash_table[aux.first] = [ Hash[aux[3].to_i ,] [aux[2].to_i] ] #[aux[1],[aux[2].to_i]] 
+           
+           @hash_table[aux.first].insert(0,1) # Inicia o contador do termo com 1
+           @hash_table[aux.first][1][aux[3].insert(0,1) # Inicia o contador do termo do documento
+
+        else # Quando já esta no indice
+   
+            # Verifica se o documento já está no hash do table
+            @hash_table[aux.first][1].has_key?(aux[3] to_i) ? @hash_table[aux.first][1][aux[3].to_i].push(aux[1.to_i]) : @hash_table[aux.first][1][aux[2].to_i] = [aux[1].to_i]
+            
+            @hash_table[aux.first][0] = @hash_table[aux.first][0] + 1 # Aumenta o contador do termo na colecao
+            @hash_table[aux.first][1][aux]                                                          # Aumenta o contaodo do termo no documento  
         end
-        return @hash_table
-    end
+
+      end
+      
+      @list.close()
+      return @hash_table
+   end
 end
--- main.rb
+++ main.rb
@@ -1,18 +1,28 @@
 require 'benchmark'
 require_relative 'data_index'
 
+def recebe_consulta()
+
+  puts "Digite a consulta a ser realizada:"
+  entrada = gets()
+  #puts entrada
+end
+
+
 def main(*args)
   
   # Termina execucao se nao receber os argumentos corretos.
   puts "O numero de argumentos esta incorreto. (esperado 1 passado(s)#{ARGV})"  unless (ARGV.length == 1)
   #Cria o indice invertido
-  puts "Tempo gasto para criar o Indice: #{Benchmark.realtime{index = Data_index.new(ARGV[0]).init_hash}}"
+  puts "Tempo gasto para criar o Indice: #{Benchmark.realtime{@index = Data_index.new(ARGV[0]).init_hash}}"
   puts "Tabela de indices criada com sucesso!"
 
+  consulta = recebe_consulta();
+
   #testes
-  #  puts "Search \"HANDSOME\" #{hash_table["HANDSOME"]}"
-  #  puts "Search \"VICTORY\" #{hash_table["VICTORY"]}"
-  #  puts "Search \"MACHINE\" #{hash_table["MACHINE"]}"
+  # puts "Search \"HANDSOME\" #{@index["HANDSOME"]}\n\n"
+  # puts "Search \"VICTORY\" #{@index["VICTORY"]}\n\n"
+  # puts "Search \"MACHINE\" #{@index["MACHINE"]}\n\n"
 
 end
 
